36124a28af232915d452e4c20173df9743934d6f
Adam Kulawik <adamkulawik@wp.pl>
2018-11-12 18:23:46 +0100

    RestCurrentDemandProvider made configurable. Dummy properties added

--- delivery-planning-adapters/src/main/java/io/dddbyexamples/delivery/planning/plan/RestCurrentDemandProvider.java
+++ delivery-planning-adapters/src/main/java/io/dddbyexamples/delivery/planning/plan/RestCurrentDemandProvider.java
@@ -6,30 +6,40 @@ import io.vavr.CheckedFunction0;
 import io.vavr.control.Try;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.util.List;
 
+import static java.time.Duration.ofMillis;
 import static java.util.Collections.emptyList;
 
 class RestCurrentDemandProvider implements CurrentDemandProvider {
 
     private final RestTemplate restTemplate;
+    private final String demandsUrl;
+    private final int retries;
+    private final long waitDuration;
 
-    RestCurrentDemandProvider(RestTemplate restTemplate) {
+    RestCurrentDemandProvider(RestTemplate restTemplate,
+                              @Value("${rest.url.demamds}") String demandsUrl,
+                              @Value("${rest.retries}") int retries,
+                              @Value("${rest.timeout}") long waitDuration) {
         this.restTemplate = restTemplate;
+        this.demandsUrl = demandsUrl;
+        this.retries = retries;
+        this.waitDuration = waitDuration;
     }
 
     @Override
     public List<CurrentDemand> getByDate(LocalDate date) {
 
         RetryConfig config = RetryConfig.custom()
-                .maxAttempts(3)
-                .waitDuration(Duration.ofMillis(500))
+                .maxAttempts(retries)
+                .waitDuration(ofMillis(waitDuration))
                 .build();
-        Retry retry = Retry.of("id", config);
+        Retry retry = Retry.of("demands", config);
 
         CheckedFunction0<Demands> retryableSupplier = Retry.decorateCheckedSupplier(retry, () -> fetchDemands(date));
 
@@ -39,7 +49,7 @@ class RestCurrentDemandProvider implements CurrentDemandProvider {
     }
 
     private Demands fetchDemands(LocalDate date) {
-        return restTemplate.getForObject("url", Demands.class);
+        return restTemplate.getForObject(demandsUrl, Demands.class, date);
     }
 
     @Getter
@@ -49,5 +59,4 @@ class RestCurrentDemandProvider implements CurrentDemandProvider {
 
         private List<CurrentDemand> demands;
     }
-
 }
